2a20bbaa01307361da884c75615c46bf7938e094
Mariya Zakharova <zakharovamaria73@yandex.ru>
2023-10-19 19:06:02 +0400

    start

---
+++ .vscode/cch-template/FC/{{pascalCase}}/{{pascalCase}}.tsx
@@ -0,0 +1,12 @@
+import React, { type FC } from 'react';
+import cn from 'classnames';
+import s from './{{pascalCase}}.module.css';
+import { {{pascalCase}}Props } from './{{pascalCase}}.props';
+
+const {{pascalCase}}: FC<{{pascalCase}}Props> = () => {
+  return (
+    <div className={}></div>
+  )
+}
+
+export default {{pascalCase}};
---
+++ components/Button/Button.tsx
@@ -0,0 +1,14 @@
+import React, { type FC } from 'react';
+import cn from 'classnames';
+import s from './Button.module.css';
+import { ButtonProps } from './Button.props';
+
+const Button: FC<ButtonProps> = () => {
+  return (
+    <div className={}>
+      
+    </div>
+  )
+}
+
+export default Button;
