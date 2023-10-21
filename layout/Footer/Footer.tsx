1bc398661769b2dd5bcafbf7627feff7c85eeaf3
Mariya Zakharova <zakharovamaria73@yandex.ru>
2023-10-21 23:42:35 +0400

    getStaticProps

--- layout/Footer/Footer.tsx
+++ layout/Footer/Footer.tsx
@@ -1,14 +1,15 @@
 import React, { type FC } from 'react';
 import cn from 'classnames';
 import s from './Footer.module.css';
+import { format } from 'date-fns'
 import { FooterProps } from './Footer.props';
 
 const Footer: FC<FooterProps> = ({ className, ...props }) => {
   return (
-    <footer {...props} className={cn(className, s.wrapper)}>
-      <p>1</p>
-      <p>2</p>
-      <p>3</p>
+    <footer {...props} className={cn(className, s.footer)}>
+      <p>NextTopApp © {format(new Date(), 'yyyy')} Все права защищены</p>
+      <a className={s.link} href='#' target='_blank'>Пользовательское соглашение</a>
+      <a className={s.link} href='#' target='_blank'>Политика конфиденциальности</a>
     </footer>
   );
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -6,9 +6,32 @@ import Tag from '@/components/Tag/Tag';
 import Rating from '@/components/Rating/Rating';
 import { useState } from 'react';
 import { withLayout } from './../layout/Layout';
+import { GetStaticProps } from 'next';
+import axios from 'axios';
+import { MenuItem } from '@/interfaces/menu.interface';
 
-function Home() {
+export interface HomeProps extends Record<string, unknown> {
+  menu: MenuItem[];
+  firstCategory: number;
+}
+
+export const getStaticProps: GetStaticProps<HomeProps> = async () => {
+  const firstCategory = 0;
+  const { data: menu } = await axios.post<MenuItem[]>(
+    process.env.NEXT_PUBLIC_DOMAIN + '/api/top-page/find',
+    { firstCategory }
+  );
+  return {
+    props: {
+      menu,
+      firstCategory
+    }
+  };
+};
+
+function Home({ menu }: HomeProps) {
   const [rating, setRating] = useState<number>(4);
+  console.log(menu);
   return (
     <>
       <Head>
